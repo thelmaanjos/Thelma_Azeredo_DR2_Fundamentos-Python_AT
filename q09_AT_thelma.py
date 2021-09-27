6960bd7f0bdb76aaac827c13f55a359181b9ff09
unknown <53521611+thelmaanjos@users.noreply.github.com>
2021-09-27 00:06:17 -0300

    Added Question 9 code

--- q09_AT_thelma.py
+++ q09_AT_thelma.py
@@ -1,7 +1,8 @@
 import pygame
 from random import randint
 
-class Question_08():
+
+class Question_09():
 
     def __init__(self):
         pygame.init()
@@ -20,49 +21,74 @@ class Question_08():
         self.circle_caption = self.font.render("Clique", True, self.text_color)
         self.display_name = pygame.display.set_caption('Question 07')
 
-    def draw_button(self):
-        return pygame.draw.circle(self.screen, self.button_color, (self.circle_pos, self.circle_width), self.circle_height)
+    def draw_button(self, x, y):
+        return pygame.draw.circle(self.screen, self.button_color, (x, y), self.circle_height)
 
     def draw_random_squares(self, screen, rect_color, area):
         return pygame.draw.rect(screen, rect_color, area)
 
+    def draw_text(self):
+        return self.screen.blit(self.circle_caption, (self.circle_pos - 25, self.circle_width - 10))
+
+    def move_button(self, event):
+        if event.type == pygame.KEYDOWN:
+            self.screen.fill(self.bgcolor)
+            self.draw_button(self.circle_pos, self.circle_width)
+            self.draw_text()
+            # self.screen.blit(self.circle_caption, (self.circle_pos - 25, self.circle_width - 10))
+            for r in self.collided_rects:
+                if r.collidepoint(self.circle_pos, self.circle_width):
+                    self.collided_rects.remove(r)
+                else:
+                    self.draw_random_squares(self.screen, self.rect_color, r)
+            if event.key == ord('a'):
+                self.circle_pos -= 10
+                # print('a pressed')
+            if event.key == ord('d'):
+                self.circle_pos += 10
+                # print('d pressed')
+            if event.key == ord('w'):
+                self.circle_width -= 10
+                # print('w pressed')
+            if event.key == ord('s'):
+                self.circle_width += 10
+                # print('s pressed')
+            print(self.circle_pos, self.circle_width)
+        return self
+
     def event(self):
         self.screen.fill(self.bgcolor)
-        button = self.draw_button()
+        button = self.draw_button(self.circle_pos, self.circle_width)
         self.screen.blit(self.circle_caption, (272, 40))
-
         while not self.finish:
             x, y = randint(0, self.width), randint(0, self.height)
             area = pygame.Rect(x, y, self.rect_width, self.rect_height)
             for event in pygame.event.get():
+
+                self.move_button(event)
+
                 if event.type == pygame.QUIT:
                     self.finish = True
-                elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
-                    pos = pygame.mouse.get_pos()
-                    if button.collidepoint(pos):
+                if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+                    # pos = pygame.mouse.get_pos()
+                    cx, cy = pygame.mouse.get_pos()
+                    if (self.circle_pos + self.circle_height >= cx >= self.circle_pos - self.circle_height) and\
+                            (self.circle_width + self.circle_height >= cy >= self.circle_width - self.circle_height):
+                    # if button.collidepoint(pos):
                         square = self.draw_random_squares(self.screen, self.rect_color, area)
                         self.collided_rects.append(square)
-                        print(self.collided_rects)
-                    
-                        for r in self.collided_rects:
-                            if r is not square and r.colliderect(square):
-                                self.collided_rects.remove(r)
-                                print("colidiu")
-                                
-                                if square in self.collided_rects:
-                                    self.collided_rects.remove(square)
-                                    print("colidiu")
-                                
-                                for r in self.collided_rects:
-                                    self.screen.fill(self.bgcolor)
-                                    self.draw_random_squares(self.screen, self.rect_color, area)
-                                    self.draw_button()
-                                    self.screen.blit(self.circle_caption, (272, 40))
-                            
-                            
+                        # self.collided_rects.append(area)
+
+                        # for r in self.collided_rects:
+                        #     if r is not area and r.colliderect(area):
+                        #         self.collided_rects.remove(r)
+                        #
+                        #         if area in self.collided_rects:
+                        #             self.collided_rects.remove(area)
+
                 pygame.display.update()
 
         pygame.display.quit()
         pygame.quit()
 
-Question_08().event()
+Question_09().event()
